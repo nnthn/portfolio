ce53b354a3110bbbbb501e163757a35e677c1c79
Ananthakrishnan <ananthan@pop-os.localdomain>
2023-12-19 22:03:21 +0530

    Made the website responsivec

--- script.js
+++ script.js
@@ -10,11 +10,27 @@ function isactive(className){
     }
     return false;
 }
+function updateBackgroundImage(sectionId, imageUrl) {
+        const section = document.getElementById(sectionId);
+        section.style.backgroundImage = `url('${imageUrl}')`;
+    }
+
+function resetBackgroundImage() {
+    const sections =["shome","sskills","sabout","sproject","scontact"];
+    for (const section of sections){
+        const sectio = document.getElementById(section);
+        sectio.style.backgroundImage = `url('./assets/${section}.png')`;
+    }
+ }
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('DOM content loaded.');
-  setInterval(function () {
+    console.log('DOM content loaded.');
+    const sections =["shome","sskills","sabout","sproject","scontact"];
+    setInterval(function () {
+
+
+	
 	console.log('Yes');
 	const homeel= document.getElementById("shome");
 	const aboutel= document.getElementById("sabout");
@@ -23,16 +39,20 @@ document.addEventListener('DOMContentLoaded', function () {
 	const projectel= document.getElementById("sproject");
 	if(isactive('home')){
 	    console.log("its home");
-	   
+	    
 	    homeel.style.color="#1e88e5";
 	    aboutel.style.color="white";
 	    skillel.style.color="white";
 	    contactel.style.color="white";
 	    projectel.style.color="white";
+	    resetBackgroundImage()
+	    updateBackgroundImage('shome', './assets/achome.png');
 	}
 	else if(isactive('about')){
 	    console.log("its about");
-	     homeel.style.color="white";
+	    resetBackgroundImage();
+	    updateBackgroundImage('sabout', './assets/acabout.png');
+	    homeel.style.color="white";
 	    aboutel.style.color="#1e88e5";
 	    skillel.style.color="white";
 	    contactel.style.color="white";
@@ -40,6 +60,8 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 	else if(isactive('skills')){
 	    console.log("its skills");
+	    resetBackgroundImage();
+	    updateBackgroundImage('sskills', './assets/acskills.png');
 	    homeel.style.color="white";
 	    aboutel.style.color="white";
 	    skillel.style.color="#1e88e5";
@@ -48,6 +70,8 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 	else if(isactive('contact')){
 	    console.log("its contact");
+	    resetBackgroundImage();
+	    updateBackgroundImage('scontact', './assets/accontact.png');
 	    homeel.style.color="white";
 	    aboutel.style.color="white";
 	    skillel.style.color="white";
@@ -57,6 +81,8 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 	else if(isactive('project')){
 	    console.log("its projecct");
+	    resetBackgroundImage();
+	    updateBackgroundImage('sproject', './assets/acproject.png');
 	    homeel.style.color="white";
 	    aboutel.style.color="white";
 	    skillel.style.color="white";
