09062424d8d73c68ea7034dd3a56095ec654f87e
Ananthakrishnan <ananthan@pop-os.localdomain>
2024-03-10 01:24:30 +0530

    Navbar completed :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import Socials from "./components/Socials.jsx";
-import Navbar from "./components/Navbar.jsx";
+import "./components/navbar.css";
 import "./App.css";
 import downloadBtn from "./assets/download.svg";
+import 'intersection-observer';
+import {useState ,useEffect } from "react";
 export default function App(){
+    const [activeSection, setActiveSection] = useState(null);
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.5,
+        };
+        const callback = (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    setActiveSection(entry.target.id);
+                }
+            });
+        };
+
+        const observer = new IntersectionObserver(callback, options);
+        document.querySelectorAll('section').forEach((section) => {
+            observer.observe(section);
+        });
+
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
+
     return (
         <div>
-          <Navbar />
+          <aside>
+            <nav>
+              <ul className="sidebar-list">
+                <li className={activeSection === 'home' ? 'active' : ''}><a className="shome sidebar-a" id="shome" href="#home" ><span className="nav-indicator"></span><span className="nav-text">Home</span></a></li>
+                <li className={activeSection === 'about' ? 'active' : ''}><a className="sabout sidebar-a" id="sabout" href="#about" ><span className="nav-indicator"></span><span className="nav-text">About</span></a></li>
+                <li className={activeSection === 'skills' ? 'active' : ''}><a className="sskills sidebar-a"  id="sskills" href="#skills"><span className="nav-indicator"></span><span className="nav-text">Skills</span></a></li>
+                <li className={activeSection === 'project' ? 'active' : ''}><a className="sproject sidebar-a" id="sproject" href="#project" ><span className="nav-indicator"></span><span className="nav-text">Projects</span></a></li>
+                <li className={activeSection === 'blogs' ? 'active' : ''}><a className="sblogs sidebar-a" id="sblogs" href="#blogs" ><span className="nav-indicator"></span><span className="nav-text">Blogs</span></a></li>
+              </ul>
+            </nav>
+          </aside>  
           <main className="content">
             <section className="home" id="home">
               <h3 className="desig">I'm</h3>
