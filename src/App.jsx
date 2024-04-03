1d90c525a5dae2b3f633c1acbb19b023d649c577
Ananthakrishnan <ananthan@pop-os.localdomain>
2024-04-03 13:00:25 +0530

    some udpations on home page

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,8 @@ export default function App(){
               <h3 className="desig">I'm</h3>
               <h2 className="heading">Ananthakrishnan P</h2>
               <p className="desc-p">
-                I'm a beginner software developer with a knack for creating cool stuff. I've dabbled in designing and programming.
+                I'm a beginner software developer with a knack for creating cool stuff.
+                <br/><ul> <li>Designer</li><li>Developer</li></ul>
               </p>
               <a href={Resume} download><button className="resume-btn">Resume<img src={downloadBtn} alt="download btn"/></button></a>
             </section>
@@ -25,7 +26,7 @@ export default function App(){
               <h3 className="desig">Hi, I'm</h3>
               <h2 className="heading" id="nnthn"></h2>
               <p className="desc-p">
-               I'm a B-Tech Computer Science student from Kerala. I am a beginner software developer, mostly focused in frontend and fullstack development. I am passionate towards development of websites and webapps. I am also a beginner at other languages as C, Java, Javascript and SQL. I have some knowledge on frameworks on javascript like ReactJS, ExpressJS as well. I have done some projects that include a small Hotel Management app and a Billing app. The Hotel Management app was a web based app and the Billing app was an android app. 
+               I'm a B-Tech Computer Science student from Kerala. I am a beginner software developer, mostly focused in frontend and fullstack development. I am passionate towards development of websites and webapps. I am also a beginner at other languages as C, Java, Javascript and SQL. I have some knowledge on frameworks on javascript like ReactJS, ExpressJS as well. 
               </p>
             </section>
             <section className="skills" id="skills">
